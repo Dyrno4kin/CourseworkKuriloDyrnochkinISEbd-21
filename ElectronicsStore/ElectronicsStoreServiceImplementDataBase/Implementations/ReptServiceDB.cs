b1a283c24560abea748231d13bb8f5e369504e63
Dyrno4kin <dyrno4kin@gmail.com>
2019-06-19 18:40:06 +0400

    addClientReportPoructPriceFromXls

--- ElectronicsStore/ElectronicsStoreServiceImplementDataBase/Implementations/ReptServiceDB.cs
+++ ElectronicsStore/ElectronicsStoreServiceImplementDataBase/Implementations/ReptServiceDB.cs
@@ -32,30 +32,25 @@ namespace ElectronicsStoreServiceImplementDataBase.Implementations
         public void SaveProductPriceXls(ReptBindingModel model)
         {
             var excel = new Microsoft.Office.Interop.Excel.Application();
-            /*
             try
             {
                 //или создаем excel-файл, или открываем существующий
                 if (File.Exists(model.FileName))
                 {
-                    excel.Workbooks.Open(model.FileName, Type.Missing, Type.Missing,
-                   Type.Missing,
-                    Type.Missing, Type.Missing, Type.Missing, Type.Missing,
-                   Type.Missing,
-                    Type.Missing, Type.Missing, Type.Missing, Type.Missing,
-                   Type.Missing,
-                    Type.Missing);
+                    excel.Workbooks.Open(model.FileName, Type.Missing, Type.Missing, Type.Missing,
+                        Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing,
+                        Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing,
+                        Type.Missing);
                 }
                 else
                 {
                     excel.SheetsInNewWorkbook = 1;
                     excel.Workbooks.Add(Type.Missing);
-                    excel.Workbooks[1].SaveAs(model.FileName, XlFileFormat.xlExcel8,
-                    Type.Missing,
-                     Type.Missing, false, false, XlSaveAsAccessMode.xlNoChange,
-                    Type.Missing,
-                     Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+                    excel.Workbooks[1].SaveAs(model.FileName, XlFileFormat.xlExcel8, Type.Missing,
+                        Type.Missing, false, false, XlSaveAsAccessMode.xlNoChange, Type.Missing,
+                        Type.Missing, Type.Missing, Type.Missing, Type.Missing);
                 }
+
                 Sheets excelsheets = excel.Workbooks[1].Worksheets;
                 //Получаем ссылку на лист
                 var excelworksheet = (Worksheet)excelsheets.get_Item(1);
@@ -66,74 +61,38 @@ namespace ElectronicsStoreServiceImplementDataBase.Implementations
                 excelworksheet.PageSetup.CenterHorizontally = true;
                 excelworksheet.PageSetup.CenterVertically = true;
                 //получаем ссылку на первые 3 ячейки
-                Microsoft.Office.Interop.Excel.Range excelcells =
-               excelworksheet.get_Range("A1", "C1");
+                Microsoft.Office.Interop.Excel.Range excelcells = excelworksheet.get_Range("A1", "C1");
                 //объединяем их
                 excelcells.Merge(Type.Missing);
                 //задаем текст, настройки шрифта и ячейки
                 excelcells.Font.Bold = true;
                 excelcells.Value2 = "Список товаров";
                 excelcells.RowHeight = 25;
-                excelcells.HorizontalAlignment =
-               Microsoft.Office.Interop.Excel.XlHAlign.xlHAlignCenter;
-                excelcells.VerticalAlignment =
-           Microsoft.Office.Interop.Excel.XlVAlign.xlVAlignCenter;
+                excelcells.HorizontalAlignment = Microsoft.Office.Interop.Excel.XlHAlign.xlHAlignCenter;
+                excelcells.VerticalAlignment = Microsoft.Office.Interop.Excel.XlVAlign.xlVAlignCenter;
                 excelcells.Font.Name = "Times New Roman";
                 excelcells.Font.Size = 14;
+
                 excelcells = excelworksheet.get_Range("A2", "C2");
                 excelcells.Merge(Type.Missing);
-                excelcells.Value2 = "на" + DateTime.Now.ToShortDateString();
+                excelcells.Value2 = "на " + DateTime.Now.ToShortDateString();
                 excelcells.RowHeight = 20;
-                excelcells.HorizontalAlignment =
-               Microsoft.Office.Interop.Excel.XlHAlign.xlHAlignCenter;
-                excelcells.VerticalAlignment =
-               Microsoft.Office.Interop.Excel.XlVAlign.xlVAlignCenter;
+                excelcells.HorizontalAlignment = Microsoft.Office.Interop.Excel.XlHAlign.xlHAlignCenter;
+                excelcells.VerticalAlignment = Microsoft.Office.Interop.Excel.XlVAlign.xlVAlignCenter;
                 excelcells.Font.Name = "Times New Roman";
                 excelcells.Font.Size = 12;
+
                 var dict = context.Products.ToList();
-                if (dict != null)
+                for (int i = 0; i < dict.Count; i++)
                 {
                     excelcells = excelworksheet.get_Range("C1", "C1");
-                    foreach (var elem in dict)
-                    {
-                        //спускаемся на 2 ячейку вниз и 2 ячейкт влево
-                        excelcells = excelcells.get_Offset(2, -2);
-                        excelcells.ColumnWidth = 15;
-                        excelcells.Value2 = elem.StorageName;
-                        excelcells = excelcells.get_Offset(1, 1);
-                        //обводим границы
-                        if (elem.Ingredients.Count() > 0)
-                        {
-                            //получаем ячейкт для выбеления рамки под таблицу
-                            var excelBorder =
-                             excelworksheet.get_Range(excelcells,
-                             excelcells.get_Offset(elem..Ingredients.Count()
-                            - 1, 1));
-                            excelBorder.Borders.LineStyle =
-                           Microsoft.Office.Interop.Excel.XlLineStyle.xlContinuous;
-                            excelBorder.Borders.Weight =
-                           Microsoft.Office.Interop.Excel.XlBorderWeight.xlThin;
-                            excelBorder.HorizontalAlignment = Constants.xlCenter;
-                            excelBorder.VerticalAlignment = Constants.xlCenter;
-                            excelBorder.BorderAround(Microsoft.Office.Interop.Excel.XlLineStyle.xlContinuous,
-
-                            Microsoft.Office.Interop.Excel.XlBorderWeight.xlMedium,
-
-                            Microsoft.Office.Interop.Excel.XlColorIndex.xlColorIndexAutomatic,
-                             1);
-                            foreach (var listElem in elem.Ingredients)
-                            {
-                                excelcells.Value2 = listElem.Item1;
-                                excelcells.ColumnWidth = 10;
-                                excelcells.get_Offset(0, 1).Value2 = listElem.Item2;
-                                excelcells = excelcells.get_Offset(1, 0);
-                            }
-                        }
-                        excelcells = excelcells.get_Offset(0, -1);
-                        excelcells.Font.Bold = true;
-                        excelcells = excelcells.get_Offset(0, 2);
-                        excelcells.Font.Bold = true;
-                    }
+                    excelcells = excelcells.get_Offset(i + 2, -2);
+                    excelcells.ColumnWidth = 15;
+                    excelcells.Value2 = dict[i].ProductName;
+                    excelcells = excelcells.get_Offset(0, 1);
+                    excelcells.ColumnWidth = 15;
+                    excelcells.Value2 = dict[i].Price;
+                    excelcells.Font.Bold = true;
                 }
                 //сохраняем
                 excel.Workbooks[1].Save();
@@ -147,7 +106,6 @@ namespace ElectronicsStoreServiceImplementDataBase.Implementations
                 //закрываем
                 excel.Quit();
             }
-                */
         }
 
         public void SaveProductPriceW(ReptBindingModel model)
